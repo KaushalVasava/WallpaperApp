32be65347253673462fc17fdd9b32b0c00ce9b84
KauhsalVasava <vasavakaushal333@gmail.com>
2022-04-13 15:45:00 +0530

    Full screen now working for wallpaper

--- app/src/main/java/com/lahsuak/apps/wallpaperapp/util/ImageAdapter.kt
+++ app/src/main/java/com/lahsuak/apps/wallpaperapp/util/ImageAdapter.kt
@@ -1,5 +1,7 @@
 package com.lahsuak.apps.wallpaperapp.util
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,27 +14,59 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.lahsuak.apps.wallpaperapp.R
 import com.lahsuak.apps.wallpaperapp.model.ImageModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class ImageAdapter(private val listener: ImageListener) : PagingDataAdapter<ImageModel,
         ImageAdapter.MyViewHolder>(DiffUtilCallBack()) {
 
+
+    /** Download image from url without any library
+        private fun downloadImage(imageUrl: String): Bitmap?{
+            return try {
+                val conn = URL(imageUrl).openConnection()
+                conn.connect()
+                val inputStream = conn.getInputStream()
+                val bitmap = BitmapFactory.decodeStream(inputStream)
+                inputStream.close()
+                bitmap
+            }catch (e: Exception){
+                Log.e("TAG", "downloadImage: ${e.message}")
+                null
+            }
+        }
+    */
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val item = getItem(position)
-        Log.d("TAG", "onBindViewHolder: $item ")
-        holder.itemView.setOnClickListener {
-            if (item != null)
-                listener.onItemClick(item.urls.regular)
-        }
-        if(item == null){
-            Log.d("TAG", "onBindViewHolder: null ")
-        }
-        else {
-            Log.d("TAG", "onBindViewHolder: notnull ")
-            Glide.with(holder.itemView)
-                .load(item.urls.regular)
-                .error(R.drawable.ic_error_image)
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(holder.imageView)
+
+        try {
+            val item = getItem(position)
+            holder.itemView.setOnClickListener {
+                if (item != null)
+                    listener.onItemClick(item.urls.regular)
+            }
+            if(item!=null) {
+                /** Download image from url without any library
+
+                    CoroutineScope(Dispatchers.IO).launch{
+                        val bitmap =  downloadImage(item.urls.regular)
+                        withContext(Dispatchers.Main){
+                            if(bitmap!=null)
+                                holder.imageView.setImageBitmap(bitmap)
+                        }
+                    }
+                 */
+
+                Glide.with(holder.itemView)
+                    .load(item.urls.regular)
+                    .error(R.drawable.ic_error_image)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(holder.imageView)
+            }
+        }catch(e: Exception){
+            Log.d("TAG", "onBindViewHolder: ${e.message} ")
         }
     }
 
